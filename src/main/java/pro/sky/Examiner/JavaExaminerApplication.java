db51cdf07ebf010e700bc9699ffc4bf71ea5f75d
qmargo23 <elen.asplund@gmail.com>
2023-09-10 13:33:57 +0300

    init

---
+++ src/main/java/pro/sky/Examiner/JavaExaminerApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Examiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExaminerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaExaminerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Examiner/JavaExaminerApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Examiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExaminerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
